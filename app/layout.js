2d9dc4c43eebaa39fc505204c3ad1f3f7540dfd6
Pritam Sharma <notpritamsharma@gmail.com>
2024-01-09 16:40:08 +0530

    Initial Setup

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-
-import { cn } from "../@/lib/utils";
+import { cn } from "@/lib/utils";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
